cd1de3bb3f731feae5231ff25397cd16b17dc383
TharllesKawa <tharlleskawa22@gmail.com>
2022-09-12 10:38:59 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const toggle = document.getElementById("toggle");
+const refresh = document.getElementById("refresh");
+const theme = window.localStorage.getItem("theme");
+
+if (theme === "dark") document.body.classList.add("dark");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  if (theme === "dark") {
+    window.localStorage.setItem("theme", "light");
+  } else window.localStorage.setItem("theme", "dark");
+});
+
+refresh.addEventListener("click", () => {
+  window.location.reload();
+});
